6dcc9ecb1c82049a914dd792609e19e390c75245
hayleyxyo <haiqianzhang@HAIQIANs-MacBook-Air.local>
2016-12-20 17:29:33 +1100

    updated

--- js/index.js
+++ js/index.js
@@ -2,39 +2,40 @@ $(document).ready(function(){ //(document).ready只需要apply一次，should be
   $("a").click(function(event){ //这个可以是"a"或者"readmore"。根据需要决定
     event.preventDefault();
   });
-});
 
-//以下是read more
- $(".readmore").click(function(){
-  $("#show-this-on-click").slideDown("slow");
-});
+  //以下是read more
+   $(".readmore").click(function(){
+    $("#show-this-on-click").slideDown("slow");
+  });
 
-$(".readmore").click(function(){
-  $(this).hide();
-});
+  $(".readmore").click(function(){
+    $(this).hide();
+  });
 
-$(".readmore").click(function(){
-  $(".readless").show();
-});
+  $(".readmore").click(function(){
+    $(".readless").show();
+  });
 
-//以下是readless
-$(".readless").click(function(){
-  $("#show-this-on-click").slideUp("slow");
-});
+  //以下是readless
+  $(".readless").click(function(){
+    $("#show-this-on-click").slideUp("slow");
+  });
 
-$(".readless").click(function(){
-  $(this).hide();
-});
+  $(".readless").click(function(){
+    $(this).hide();
+  });
 
-$(".readless").click(function(){
-  $(".readmore").show();
-});
+  $(".readless").click(function(){
+    $(".readmore").show();
+  });
 
-//一下是 Learn More
-$(".learnmore").click(function(){
-  $("#learnmoretext").slideDown("slow");
-});
+  //一下是 Learn More
+  $(".learnmore").click(function(){
+    $("#learnmoretext").slideDown("slow");
+  });
 
-$(".learnmore").click(function(){
-  $(this).hide();
+  $(".learnmore").click(function(){
+    $(this).hide();
+  });
+
 });
